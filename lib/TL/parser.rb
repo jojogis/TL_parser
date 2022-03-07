a12c0365e19e4d045907e630b1f7505905dcc615
jojogis <jojogis.lesha@yandex.ru>
2022-03-07 18:55:32 +0300

    Добавлены начальные тесты

--- lib/TL/parser.rb
+++ lib/TL/parser.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "parser/version"
+require 'open-uri'
+require 'nokogiri'
+require 'json'
 
 module TL
   module Parser
@@ -19,14 +22,14 @@ module TL
         offers_link += "&criterions[0].hotels[0].code=#{hotel_id}"
         offers_link += "&criterions[0].dates=#{start_date.strftime("%Y-%m-%d")};#{finish_date.strftime("%Y-%m-%d")}&criterions[0].adults=#{adults_count}"
         response = get_js(offers_link)
-        return nil unless response.class.in?([Net::HTTPSuccess, Net::HTTPFound, Net::HTTPOK])
-        p JSON.parse(response.body)
+        return nil unless [Net::HTTPSuccess, Net::HTTPFound, Net::HTTPOK].include?(response.class)
+        JSON.parse(response.body)
       end
 
       private
         def get_hotel_id(login)
           response = get_js("https://ibe.tlintegration.com/integration/profiles/TL-INT-#{login}.js")
-          return nil unless response.class.in?([Net::HTTPSuccess, Net::HTTPFound, Net::HTTPOK])
+          return nil unless [Net::HTTPSuccess, Net::HTTPFound, Net::HTTPOK].include?(response.class)
           script = response.body
           login_start = script.index('providers') + 14
           login_end = script.index(',', login_start) - 2
@@ -41,8 +44,8 @@ module TL
         end
 
         def load_page(url)
-          html = open(url)
-          Nokogiri::HTML(html)
+          html = URI.open(url)
+          ::Nokogiri::HTML(html)
         end
 
         def get_js(url)
--- spec/TL/parser_spec.rb
+++ spec/TL/parser_spec.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
+require 'date'
 
 RSpec.describe TL::Parser do
   it "has a version number" do
     expect(TL::Parser::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "can get offers from sanpriboy" do
+    result = TL::Parser::load_offers(url: 'https://sanpriboy.ru/', start_date: Date.today.next_month, finish_date: Date.today.next_month)
+    expect(result).to be_a_kind_of(Hash)
+    expect(result).to have_key("room_stays")
+  end
+
+  it "can get offers from admiral-klub" do
+    result = TL::Parser::load_offers(url: 'https://admiral-klub.ru', start_date: Date.today.next_month, finish_date: Date.today.next_month)
+    expect(result).to be_a_kind_of(Hash)
+    expect(result).to have_key("room_stays")
   end
 end
