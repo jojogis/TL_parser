c4593851ac8f8f1f955259933da56f92b3bffe13
jojogis <jojogis.lesha@yandex.ru>
2022-03-07 20:05:07 +0300

    Добавлены тесты

--- lib/TL/parser/version.rb
+++ lib/TL/parser/version.rb
@@ -2,6 +2,6 @@
 
 module TL
   module Parser
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
--- spec/TL/parser_spec.rb
+++ spec/TL/parser_spec.rb
@@ -8,18 +8,61 @@ RSpec.describe TL::Parser do
   end
 
   it "can get offers from sanpriboy" do
-    result = TL::Parser.load_offers(url: "https://sanpriboy.ru/",
+    result = TL::Parser.load_offers(url: "https://sanpriboy.ru",
                                     start_date: Date.today.next_month,
-                                    finish_date: Date.today.next_month)
+                                    finish_date: Date.today.next_month.next_day)
     expect(result).to be_a_kind_of(Hash)
     expect(result).to have_key("room_stays")
+    expect(result).to have_key("transfers")
+    expect(result).to have_key("services")
+    expect(result).to have_key("availability_result")
+    expect(result).to have_key("room_type_quotas")
   end
 
   it "can get offers from admiral-klub" do
     result = TL::Parser.load_offers(url: "https://admiral-klub.ru",
                                     start_date: Date.today.next_month,
-                                    finish_date: Date.today.next_month)
+                                    finish_date: Date.today.next_month.next_day)
     expect(result).to be_a_kind_of(Hash)
     expect(result).to have_key("room_stays")
+    expect(result).to have_key("transfers")
+    expect(result).to have_key("services")
+    expect(result).to have_key("availability_result")
+    expect(result).to have_key("room_type_quotas")
+  end
+
+  it "can get offers from dona-rosa" do
+    result = TL::Parser.load_offers(url: "https://dona-rosa.ru",
+                                    start_date: Date.today.next_month,
+                                    finish_date: Date.today.next_month.next_day)
+    expect(result).to be_a_kind_of(Hash)
+    expect(result).to have_key("room_stays")
+    expect(result).to have_key("transfers")
+    expect(result).to have_key("services")
+    expect(result).to have_key("availability_result")
+    expect(result).to have_key("room_type_quotas")
+  end
+
+  it "can get offers from ghkandt" do
+    result = TL::Parser.load_offers(url: "https://ghkandt.com/",
+                                    start_date: Date.today.next_month,
+                                    finish_date: Date.today.next_month.next_day)
+    expect(result).to be_a_kind_of(Hash)
+    expect(result).to have_key("room_stays")
+    expect(result).to have_key("transfers")
+    expect(result).to have_key("services")
+    expect(result).to have_key("availability_result")
+    expect(result).to have_key("room_type_quotas")
+  end
+
+  it "it returns warning when input nights is zero" do
+    result = TL::Parser.load_offers(url: "https://admiral-klub.ru",
+                                    start_date: Date.today.next_month,
+                                    finish_date: Date.today.next_month)
+    expect(result).to be_a_kind_of(Hash)
+    expect(result).to have_key("warnings")
+    expect(result["warnings"]).to be_a_kind_of(Array)
+    expect(result["warnings"]).not_to be_empty
+    expect(result["warnings"].first["message"]).to eql("Nights must be greater than zero")
   end
 end
